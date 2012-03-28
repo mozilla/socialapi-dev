f395f1288048bb91c7fe54231bb95fd2721f0c20
mixedpuppy <mixedpuppy@gmail.com>
2012-03-27 18:04:35 -0700

    move recommend button code into chrome

--- content/recommendButtonWidget.js
+++ content/recommendButtonWidget.js
@@ -1,27 +1,18 @@
 "use strict";
 
-const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://socialdev/lib/baseWidget.js");
 
-Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://socialdev/lib/registry.js");
-Cu.import("resource://socialdev/lib/baseWidget.js");
-
-const EXPORTED_SYMBOLS = ["RecommendButton"];
-
-const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
-
-
-function RecommendButton(aWindow) {
-  baseWidget.call(this, aWindow);
-  this.window = aWindow;
+function SocialRecommendButton() {
+  baseWidget.call(this, window);
 }
-RecommendButton.prototype = {
+SocialRecommendButton.prototype = {
   __proto__: baseWidget.prototype,
   create: function(aWindow) {
   },
   setProvider: function(aProvider) {
     let self = this;
-    let worker = aProvider.makeWorker(this.window);
+    let worker = aProvider.makeWorker(window);
 
     worker.port.onmessage = function(evt) {
       if (evt.data.topic === 'user-recommend-prompt-response') {
@@ -32,13 +23,13 @@ RecommendButton.prototype = {
     worker.port.postMessage({topic: "user-recommend-prompt"});
   },
   enable: function(aIconURL, aTooltiptext) {
-    let widget = this.window.document.getElementById("social-recommend-button");
+    let widget = window.document.getElementById("social-recommend-button");
     widget.setAttribute("tooltiptext", aTooltiptext); // XXX - 'message' not in spec.
     widget.setAttribute("src", aIconURL);
     widget.removeAttribute("hidden");
   },
   disable: function() {
-    let widget = this.window.document.getElementById("social-recommend-button");
+    let widget = window.document.getElementById("social-recommend-button");
     widget.setAttribute("hidden", "true");
     widget.setAttribute("tooltiptext", "");
     widget.setAttribute("src", "");
@@ -47,12 +38,13 @@ RecommendButton.prototype = {
     this.disable();
   },
   oncommand: function(event) {
-    let url = this.window.gBrowser.currentURI.cloneIgnoringRef().spec;
-    Services.console.logStringMessage("recommending "+ url);
-    let worker = providerRegistry().currentProvider.makeWorker(this.window)
+    Components.utils.import("resource://socialdev/lib/registry.js");
+    let url = window.gBrowser.currentURI.cloneIgnoringRef().spec;
+    let worker = providerRegistry().currentProvider.makeWorker(window)
     worker.port.postMessage({topic: "user-recommend",
                              data: {
                               url: url}
                             });
+    Services.console.logStringMessage("recommending "+ url);
   }
 }
