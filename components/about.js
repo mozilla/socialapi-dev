1cc8d22a7c80cd43167645ca94d55b032077852c
mixedpuppy <mixedpuppy@gmail.com>
2012-03-28 14:16:32 -0700

    fix about:social

--- components/about.js
+++ components/about.js
@@ -2,6 +2,7 @@ const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://socialdev/lib/manifestDB.jsm");
 
 const ABOUTURL = "chrome://socialdev/content/about.html";
 const EXPORTED_SYMBOLS = [];
