18e45e1066cf97aeabc8b0958811725c691a0c2f
mixedpuppy <mixedpuppy@gmail.com>
2012-04-02 15:15:53 -0700

    forgot to comit update to servicewindow.js

--- modules/servicewindow.js
+++ modules/servicewindow.js
@@ -31,6 +31,7 @@ const SOCIAL_WINDOWTYPE = "socialdev:window";
 
 function serviceWindowMaker(options) {
 
+  // xul here is unused, keeping for reference until we finish the xul files.
   var chatXul = '<?xml version="1.0"?>' +
     (isMac ? '<?xul-overlay href="chrome://browser/content/macBrowserOverlay.xul"?>' : '<?xml-stylesheet href="chrome://browser/skin/" type="text/css"?>') +
              '<window id="main-window" windowtype="' + SOCIAL_WINDOWTYPE + '" xmlns:html="'+ xhtmlNs+'" xmlns="' + xulNs + '" chromemargin="0,-1,-1,-1">';
@@ -65,12 +66,12 @@ function serviceWindowMaker(options) {
   /* We now pass the options.url, which is the user app directly
     inserting it in the window, instead using the xul browser element
     that was here. This helped to make the session history work. */
+  options.wrappedJSObject = options;
 
   let ww = Cc["@mozilla.org/embedcomp/window-watcher;1"]
     .getService(Ci.nsIWindowWatcher);
-
-  var url = "data:application/vnd.mozilla.xul+xml," + escape(chatXul);
-  var window = ww.openWindow(null, url, options.name, options.features, options.arguments);
+  var window = ww.openWindow(null, 'chrome://socialdev/content/serviceWindow.xul',
+                             options.name, options.features, options);
 
   // We catch the first DOMContentLoaded, which means the XUL
   // document has loaded.
@@ -82,22 +83,10 @@ function serviceWindowMaker(options) {
         }
       }, false);
     }
-    var browser = window.document.getElementById('browser');
-    browser.addProgressListener(SecurityStatusListener(window),
-                                Ci.nsIWebProgress.NOTIFY_ALL);
     if (options.onReady) options.onReady();
   }
 
   window.addEventListener("DOMContentLoaded", onXULReady, false);
-
-  Object.defineProperty(window, "browser", {
-    get: function() { return window.document.getElementById('browser'); }
-  });
-
-  window.loadURI = function loadURI(URI) {
-    options.url = URI;
-    browser.setAttribute('src', URI);
-  }
   return window;
 }
 
@@ -162,124 +151,6 @@ function createServiceWindow(toURL, name, options, withService, title, readyCall
   return wrapServiceWindowForContent(aWind);
 }
 
-
-function SecurityStatusListener(window) {
-  let document = window.document;
-  return {
-    _isBusy: false,
-    get statusMeter() {
-      delete this.statusMeter;
-      return this.statusMeter = document.getElementById("statusbar-icon");
-    },
-    get securityButton() {
-      delete this.securityButton;
-      return this.securityButton = document.getElementById("security-button");
-    },
-    get securityLabel() {
-      delete this.securityLabel;
-      return this.securityLabel = document.getElementById("security-status");
-    },
-    get securityDisplay() {
-      delete this.securityDisplay;
-      return this.securityDisplay = document.getElementById("security-display");
-    },
-    
-    QueryInterface: function(aIID) {
-      if (aIID.equals(Ci.nsIWebProgressListener)   ||
-          aIID.equals(Ci.nsIWebProgressListener2)  ||
-          aIID.equals(Ci.nsISupportsWeakReference) ||
-          aIID.equals(Ci.nsISupports))
-        return this;
-      throw Components.results.NS_NOINTERFACE;
-    },
-    onStateChange: function(/*in nsIWebProgress*/ aWebProgress,
-                       /*in nsIRequest*/ aRequest,
-                       /*in unsigned long*/ aStateFlags,
-                       /*in nsresult*/ aStatus) {
-      if (aStateFlags & Ci.nsIWebProgressListener.STATE_START &&
-          aStateFlags & Ci.nsIWebProgressListener.STATE_IS_NETWORK) {
-        this.statusMeter.value = 0;
-        this.statusMeter.parentNode.collapsed = false;
-        this.securityLabel.collapsed = true;
-      }
-      else if (aStateFlags & Ci.nsIWebProgressListener.STATE_STOP &&
-               aStateFlags & Ci.nsIWebProgressListener.STATE_IS_NETWORK) {
-        this.statusMeter.parentNode.collapsed = true;
-        this.securityLabel.collapsed = false;
-      }
-    },
-  
-    onProgressChange: function(/*in nsIWebProgress*/ aWebProgress,
-                          /*in nsIRequest*/ aRequest,
-                          /*in long*/ aCurSelfProgress,
-                          /*in long */aMaxSelfProgress,
-                          /*in long */aCurTotalProgress,
-                          /*in long */aMaxTotalProgress) {
-      if (aMaxTotalProgress > 0) {
-        let percentage = (aCurTotalProgress * 100) / aMaxTotalProgress;
-        this.statusMeter.value = percentage;
-      }
-    },
-  
-    onLocationChange: function(/*in nsIWebProgress*/ aWebProgress,
-                          /*in nsIRequest*/ aRequest,
-                          /*in nsIURI*/ aLocation) {
-      // XXX this needs to be cleaned up to handle differences better, the
-      // callback url should be configurable as well
-      this.securityDisplay.setAttribute('label', aLocation.host);
-    },
-  
-    onStatusChange: function(/*in nsIWebProgress*/ aWebProgress,
-                        /*in nsIRequest*/ aRequest,
-                        /*in nsresult*/ aStatus,
-                        /*in wstring*/ aMessage) {
-    },
-  
-    onSecurityChange: function(/*in nsIWebProgress*/ aWebProgress,
-                          /*in nsIRequest*/ aRequest,
-                          /*in unsigned long*/ aState) {
-      const wpl_security_bits = Ci.nsIWebProgressListener.STATE_IS_SECURE |
-                                Ci.nsIWebProgressListener.STATE_IS_BROKEN |
-                                Ci.nsIWebProgressListener.STATE_IS_INSECURE |
-                                Ci.nsIWebProgressListener.STATE_SECURE_HIGH |
-                                Ci.nsIWebProgressListener.STATE_SECURE_MED |
-                                Ci.nsIWebProgressListener.STATE_SECURE_LOW;
-      var browser = document.getElementById("browser");
-      var level;
-      
-      switch (aState & wpl_security_bits) {
-        case Ci.nsIWebProgressListener.STATE_IS_SECURE | Ci.nsIWebProgressListener.STATE_SECURE_HIGH:
-          level = "high";
-          break;
-        case Ci.nsIWebProgressListener.STATE_IS_SECURE | Ci.nsIWebProgressListener.STATE_SECURE_MED:
-        case Ci.nsIWebProgressListener.STATE_IS_SECURE | Ci.nsIWebProgressListener.STATE_SECURE_LOW:
-          level = "low";
-          break;
-        case Ci.nsIWebProgressListener.STATE_IS_BROKEN:
-          level = "broken";
-          break;
-      }
-      if (level) {
-        this.securityButton.setAttribute("level", level);
-        this.securityButton.hidden = false;
-        this.securityLabel.setAttribute("label", browser.securityUI.tooltipText);
-      }
-      else {
-        this.securityButton.hidden = true;
-        this.securityButton.removeAttribute("level");
-      }
-      this.securityButton.setAttribute("tooltiptext", browser.securityUI.tooltipText);
-    },
-    onProgressChange64: function() {
-      return this.onProgressChange(aWebProgress, aRequest,
-        aCurSelfProgress, aMaxSelfProgress, aCurTotalProgress,
-        aMaxTotalProgress);
-    },
-    onRefreshAttempted: function() {
-      return true;
-    }
-  }
-}
 
 function closeWindowsForService(aService)
 {
